3c4c08a6ff6653c1fc9d108a287723147b50c735
muhammad_saleem <160356353+Mrsaleem110@users.noreply.github.com>
2024-04-25 00:36:37 +0500

    100 days of challenge

---
+++ Q.2 personal message/main.ts
@@ -0,0 +1,5 @@
+
+// Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
+
+let personalmessage = "Muhammad saleem";
+console.log(`“Hello ${personalmessage}, would you like to learn some TypeScript today?”`);
---
+++ Q.3 name cases/index.js
@@ -0,0 +1,5 @@
+// Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+var namecases = "muhammad saleem";
+console.log(namecases.toLowerCase());
+console.log(namecases.toUpperCase());
+console.log(namecases.charAt(0).toUpperCase() + namecases.slice(1).toLowerCase());
